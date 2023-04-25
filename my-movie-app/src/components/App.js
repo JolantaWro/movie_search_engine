d4c0254700b628c4a6b04ee8aff687b14e8cb636
JolantaWro <jolanta.woronowska@gmail.com>
2023-04-25 10:05:06 +0200

    create app

---
+++ my-movie-app/src/components/App.js
@@ -0,0 +1,15 @@
+
+import '../App.css';
+import 'bulma/css/bulma.min.css'
+
+import React from 'react';
+
+const App = () => {
+  return (
+      <div>
+        <h1>Start</h1>
+      </div>
+  );
+};
+
+export default App;
